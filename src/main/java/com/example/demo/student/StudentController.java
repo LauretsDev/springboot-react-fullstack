ece399b03883836ba6ce96215f054c7516ee03c6
Diego Laurentino <diego.r.laurentino@gmail.com>
2021-04-07 09:45:03 -0400

    Added student repository and service. Changed ddl auto to update

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,21 +1,21 @@
 package com.example.demo.student;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping(path = "api/v1/students")
 public class StudentController {
 
+    private final StudentService studentService;
+
     @GetMapping
     public List<Student> getAllStudents() {
-        return Arrays.asList(
-                new Student(1L,"Diego","diego@gmail.com",Gender.MALE),
-                new Student(2L,"Penis","penis@gmail.com",Gender.MALE)
-        );
+        return studentService.getAllStudents();
     }
 }
